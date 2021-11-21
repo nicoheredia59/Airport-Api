f2905df4f256ec0e5a89789a725fc1660a613975
nicoheredia59 <nicoheredia59@gmail.com>
2021-11-21 12:59:21 -0300

    adding me resolver

--- src/resolvers/querys/user.query.ts
+++ src/resolvers/querys/user.query.ts
@@ -1,10 +1,22 @@
-import {Query, Resolver} from "type-graphql";
+import {Ctx, Query, Resolver} from "type-graphql";
+import {getRepository} from "typeorm";
 import {User} from "../../entity/user.entity";
+import {MyContext} from "../../types/MyContext";
 
 @Resolver()
 export class UserQuery {
   @Query(() => [User], {nullable: true})
   async getUsers() {
     return await User.find();
+  }
+
+  @Query(() => User, {nullable: true})
+  async me(@Ctx() {req}: MyContext) {
+    const user = await getRepository(User)
+      .createQueryBuilder("user")
+      .where(`user.email = '${req.session.email}'`)
+      .getOne();
+
+    return user;
   }
 }
