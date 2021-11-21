12d99df77e9d227107e494e4a43957e05b4d88a3
nicoheredia59 <nicoheredia59@gmail.com>
2021-11-21 12:56:26 -0300

    adding new resolvers

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,4 +1,5 @@
 import {buildSchema, NonEmptyArray} from "type-graphql";
+import {AgencyMutation} from "./mutations/agency.mutation";
 import {AirportMutation} from "./mutations/airport.mutation";
 import {FlightMutation} from "./mutations/flight.mutation";
 import {ReservationMutation} from "./mutations/reservation.mutation";
@@ -7,6 +8,7 @@ import {FlightQuery} from "./querys/flight.query";
 import {HelloResolver} from "./querys/hello";
 import {ReservationQuery} from "./querys/reservation.query";
 import {UserQuery} from "./querys/user.query";
+import {ReservationSubscription} from "./subsciptions/reservation.subscription";
 
 const resolverArr = [
   HelloResolver,
@@ -17,6 +19,8 @@ const resolverArr = [
   FlightQuery,
   ReservationMutation,
   ReservationQuery,
+  AgencyMutation,
+  ReservationSubscription,
 ] as const;
 
 export const schema_index = buildSchema({
